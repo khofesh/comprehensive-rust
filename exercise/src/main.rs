4b7fc24ac0ee9a85d4f589daff7d7656b6fe568a
fahmi ahmad <fahmiahmad@tutanota.com>
2023-01-04 23:10:41 +0700

    methods and overloading

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -1,3 +1,22 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn inc_width(&mut self, delta: u32) {
+        self.width += delta;
+    }
+}
+
+fn pick_one<T>(a: T, b: T) -> T {
+    if std::process::id() % 2 == 0 {a} else {b}
+}
+
 fn main() {
     println!("copy me!");
 
@@ -34,6 +53,20 @@ fn main() {
 
     /* functions */
     fizzbuzz_to(20);
+
+    /* methods */
+    let mut rect = Rectangle {
+        width: 10,
+        height: 5
+    };
+
+    println!("old area: {}", rect.area());
+    rect.inc_width(5);
+    println!("new area: {}", rect.area());
+
+    /* generics */
+    println!("coin toss: {}", pick_one("heads", "tails"));
+    println!("cash prize: {}", pick_one(500, 1000));
 }
 
 fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
