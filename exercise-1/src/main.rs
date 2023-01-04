b19c1093a5c36dbf830b0847055776f1f9bed6dc
fahmi ahmad <fahmiahmad@tutanota.com>
2023-01-04 23:22:07 +0700

    exercise 1 and 2

---
+++ exercise-1/src/main.rs
@@ -0,0 +1,12 @@
+fn multiply(x: i16, y: i16) -> i16 {
+    x * y
+}
+
+fn main() {
+    let x: i8 = 15;
+    let y: i16 = 1000;
+
+    // https://doc.rust-lang.org/std/convert/trait.From.html
+    println!("{x} * {y} = {}", multiply(x.into(), y));
+}
+
