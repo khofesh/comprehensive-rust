e3b17efaefd53e9add99be793339dbda547811a6
fahmi ahmad <fahmiahmad@tutanota.com>
2023-04-08 00:15:06 +0700

    variant payloads

--- day-2/enums/src/main.rs
+++ day-2/enums/src/main.rs
@@ -23,6 +23,36 @@ fn flip_coin() -> CoinFlip {
     }
 }
 
+/**
+ * variant payloads
+ */
+enum WebEvent {
+    PageLoad, // variant without payload
+    KeyPress(char), // tuple struct variant
+    Click {x: i64, y: i64}, // full struct variant
+}
+
+#[rustfmt::skip]
+fn inspect(event: WebEvent) {
+    match event {
+        WebEvent::PageLoad => println!("page loaded"),
+        WebEvent::KeyPress(c) => println!("pressed '{c}'"),
+        WebEvent::Click { x, y } => println!("clicked at x={x}, y={y}"),
+    }
+}
+
+fn second() {
+    let load = WebEvent::PageLoad;
+    let press = WebEvent::KeyPress('x');
+    let click = WebEvent::Click { x: 20, y: 80 };
+
+    inspect(load);
+    inspect(press);
+    inspect(click);
+}
+
 fn main() {
     println!("You got: {:?}", flip_coin());
+
+    second();
 }
